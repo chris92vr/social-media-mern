508cde2456e87d0155deaf884ae108e7f5539dee
chris92vr <garofolichristian@gmail.com>
2022-07-23 23:36:46 +0200

    Initial commit!

---
+++ template.js
@@ -0,0 +1,12 @@
+export default () => {
+    return `<!doctype html>
+      <html lang="en">
+          <head>
+             <meta charset="utf-8">
+             <title>Social Net </title>
+          </head>
+          <body>
+            <div id="root">Hello Guys</div>
+          </body>
+      </html>`
+}
